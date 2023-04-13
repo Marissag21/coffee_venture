cd21d8e9ac03aa66bd2c76d424035f206fc837ac
AngelToscano <115598308+AngelToscano@users.noreply.github.com>
2023-04-13 16:51:32 -0500

    updated the app js html

--- ML and connection to app/static/coffee.js
+++ ML and connection to app/static/coffee.js
@@ -1,74 +1,73 @@
-// Function to get roast type based on coffee preferences
-function getRoastType(preferences) {
-    const acidity = preferences.acidity;
-    const aroma = preferences.aroma;
-    const body = preferences.body;
-    const flavor = preferences.flavor;
-    const price = preferences.price;
-    let roast;
-
-    if (price >= 20 && body >= 8 && flavor >= 8) {
-        roast = 'Dark Roast';
-    } else if (price >= 15 && acidity >= 6 && aroma >= 6) {
-        roast = 'Medium Roast';
-    } else if (price <= 10 && acidity <= 4 && aroma >= 4) {
-        roast = 'Light Roast';
-    } else {
-        roast = 'No match found';
-    }
-
-    return roast;
-}
-
 // Function to update the slider value display
 function updateSliderValue(slider, output) {
     const sliderValue = document.getElementById(slider).value;
     document.getElementById(output).innerHTML = sliderValue;
-}
-
-// Set initial slider values
-updateSliderValue('acidity', 'acidity-value');
-updateSliderValue('aroma', 'aroma-value');
-updateSliderValue('body', 'body-value');
-updateSliderValue('flavor', 'flavor-value');
-updateSliderValue('price', 'price-value');
-
-// Update slider values when sliders are moved
-document.querySelectorAll('input[type="range"]').forEach(slider => {
+  }
+  
+  // Set initial slider values
+  updateSliderValue('acidity', 'acidity-value');
+  updateSliderValue('aftertaste', 'aftertaste-value');
+  updateSliderValue('aroma', 'aroma-value');
+  updateSliderValue('body', 'body-value');
+  updateSliderValue('flavor', 'flavor-value');
+  updateSliderValue('price', 'price-value');
+  
+  // Update slider values when sliders are moved
+  document.querySelectorAll('input[type="range"]').forEach(slider => {
     slider.addEventListener('input', () => {
-        const sliderId = slider.getAttribute('id');
-        const outputId = `${sliderId}-value`;
-        updateSliderValue(sliderId, outputId);
+      const sliderId = slider.getAttribute('id');
+      const outputId = `${sliderId}-value`;
+      updateSliderValue(sliderId, outputId);
     });
-});
-
-// Process form submission
-const form = document.getElementById('coffee-form');
-form.addEventListener('submit', event => {
+  });
+  
+  // Process form submission
+  const form = document.getElementById('coffee-form');
+  form.addEventListener('submit', event => {
     event.preventDefault();
-
+  
     const preferences = {
-        acidity: parseInt(document.getElementById('acidity').value),
-        aroma: parseInt(document.getElementById('aroma').value),
-        body: parseInt(document.getElementById('body').value),
-        flavor: parseInt(document.getElementById('flavor').value),
-        price: parseInt(document.getElementById('price').value)
+      acidity: parseInt(document.getElementById('acidity').value),
+      aftertaste: parseInt(document.getElementById('aftertaste').value),
+      aroma: parseInt(document.getElementById('aroma').value),
+      body: parseInt(document.getElementById('body').value),
+      flavor: parseInt(document.getElementById('flavor').value),
+      price: parseFloat(document.getElementById('price').value)
     };
-
-    const roastType = getRoastType(preferences);
-
-    const roastPredictionElement = document.getElementById('roast-prediction');
-    roastPredictionElement.textContent = `Based on your preferences, we recommend a ${roastType} roast.`;
-
-    const recommendationsElement = document.getElementById('recommendations');
-    if (roastType === 'Dark Roast') {
-        recommendationsElement.textContent = 'We recommend serving with chocolate or caramel-flavored desserts.';
-    } else if (roastType === 'Medium Roast') {
-        recommendationsElement.textContent = 'We recommend serving with fruity desserts or breakfast pastries.';
-    } else if (roastType === 'Light Roast') {
-        recommendationsElement.textContent = 'We recommend serving with citrus-flavored desserts or light breakfast dishes.';
-    } else {
-        recommendationsElement.textContent = 'Sorry, we do not have any serving recommendations for the roast type we recommended.';
-    }
-});
-
+  
+    // Send coffee preferences to Flask app for prediction
+    fetch('/predict', {
+      method: 'POST',
+      body: JSON.stringify(preferences),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Update the page with the predicted roast type and recommended coffees
+      document.getElementById('roast-prediction').innerHTML = `You should try ${data.dt_prediction} coffee.`;
+  
+      if (data.dt_prediction === 'Dark Roast') {
+        document.getElementById('recommendations').innerHTML = 'We recommend serving with a hearty breakfast or as a dessert coffee.';
+      } else if (data.dt_prediction === 'Medium Roast') {
+        document.getElementById('recommendations').innerHTML = 'We recommend serving with lunch or as an afternoon pick-me-up.';
+      } else if (data.dt_prediction === 'Light Roast') {
+        document.getElementById('recommendations').innerHTML = 'We recommend serving with breakfast or as a mild coffee for any time of day.';
+      } else {
+        document.getElementById('recommendations').innerHTML = '';
+      }
+  
+      // Display the top recommended coffees
+      const topCoffees = data.top_coffees;
+      let recommendationsList = '';
+      topCoffees.forEach(coffee => {
+        recommendationsList += `<li>${coffee.name} - ${coffee.roaster} (${coffee.rating} rating)</li>`;
+      });
+      document.getElementById('recommendations-list').innerHTML = recommendationsList;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  });
+  
